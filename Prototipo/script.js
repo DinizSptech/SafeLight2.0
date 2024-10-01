a74c32f54e705e010ef53fe6a8851658c4b73194
DinizSptech <mateus.leite@sptech.scho>
2024-10-01 20:27:24 -0300

    Header feito e paginas criadas, arquivo de js, não consegui fazer a transição entre as paginas funcionar, help me

---
+++ Prototipo/script.js
@@ -0,0 +1,21 @@
+function irHome(){
+    window.location.href = 'Prototipo\home.html';
+}
+function irSobre(){
+    window.location.href = 'Prototipo\Sobre nós.html';
+}
+function irCalc(){
+    window.location.href = 'Prototipo\Simulador de energia.html';
+}
+function irFale(){
+    window.location.href = 'Prototipo\Fale conosco.html';
+}
+function irFaq(){
+    window.location.href = 'Prototipo\faq.html';
+}
+function irLogin(){
+    window.location.href = 'Prototipo\Cadastro.html';
+}
+function irCadastro(){
+    window.location.href = 'Prototipo\Login.html';
+}
