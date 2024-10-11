517144f579368f333cce8f17ec346cb1f8600f4b
gabrielpacificoo <gabriel.pacifico@sptech.school>
2024-10-11 15:14:23 -0300

    Update codigoSensorLDR.ino

--- Arduino/codigoSensorLDR.ino
+++ Arduino/codigoSensorLDR.ino
@@ -1,6 +1,5 @@
 int ldr_pin = A5;
 int ldr_read = 0;
-
 float vin = 5.00;
 float valor_ADC = 0.00488758, r_ohms = 10000;
 
@@ -10,15 +9,19 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  ldr_read = analogRead(ldr_pin);
 
-  float vout = valor_ADC * ldr_read;
-  float res_ldr = (r_ohms * (vin - vout))/vout;
-  float lux = 500/(res_ldr/1000);
+  if (isnan(ldr_read)) {
+    Serial.println('Erro ao ler o sensor');
+  } else {
+    // put your main code here, to run repeatedly:
+    ldr_read = analogRead(ldr_pin);
 
-  Serial.println(ldr_read);
-  Serial.println(lux);
+    float vout = valor_ADC * ldr_read;
+    float res_ldr = (r_ohms * (vin - vout)) / vout;
+    float lux = 500 / (res_ldr / 1000);
 
+    Serial.println(lux);
+    
+  }
   delay(5000);
 }
