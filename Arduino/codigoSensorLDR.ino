fcb8dde213e6d7c138ccfaa89e50e8f86ee8f81f
gabrielpacificoo <gabriel.pacifico@sptech.school>
2024-10-11 12:45:30 -0300

    Atualização Final Código Arduíno (Lux)

--- Arduino/codigoSensorLDR.ino
+++ Arduino/codigoSensorLDR.ino
@@ -1,25 +1,24 @@
-const ldr_pin = A5; //Pino conectado ao sensor LDR
-const ldr_read = 0; //Variavel para armazenar o valor da luminosidade
+int ldr_pin = A5;
+int ldr_read = 0;
 
 float vin = 5.00;
-float valor_ADC = 0.0048875, r_ohms = 10000;
+float valor_ADC = 0.00488758, r_ohms = 10000;
 
-// Função de inicialização
 void setup() {
-Serial.begin(9600); //Inicializa a comunicação serial a 9600 bps (bits por segundo)  
-
+  // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
-// Função principal de execução contínua
 void loop() {
+  // put your main code here, to run repeatedly:
   ldr_read = analogRead(ldr_pin);
 
   float vout = valor_ADC * ldr_read;
-  float res_ldr = (r_ohms * (vin - vout)) / vout;
+  float res_ldr = (r_ohms * (vin - vout))/vout;
   float lux = 500/(res_ldr/1000);
 
+  Serial.println(ldr_read);
   Serial.println(lux);
-  
-  //Aguarda 2 segundos antes da próxima leitura
-  delay(2000);
+
+  delay(5000);
 }
