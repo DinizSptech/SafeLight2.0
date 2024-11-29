30135a5e35056514af828d1c5553ccf392560258
DinizSptech <mateus.leite@sptech.school>
2024-11-29 14:14:08 -0300

    Endereços funcionando

---
+++ Site/web-data-viz/src/controllers/empresaController.js
@@ -0,0 +1,40 @@
+var empresaModel = require("../models/empresaModel");
+
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeEmpresa;
+    var cnpj = req.body.cnpjEmpresa;
+    var codigo = req.body.codigoEmpresa;
+
+    console.log('Estou no Controller ', nome, cnpj, codigo)
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Nome da Empresa está undefined!");
+    } else if (codigo == undefined) {
+        res.status(400).send("Codigo da Empresa está undefined!");
+    } else if (cnpj == undefined) {
+        res.status(400).send("CNPJ a vincular da Empresa está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        empresaModel.cadastrar(nome, cnpj, codigo)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
+module.exports = {
+    cadastrar
+}
---
+++ Site/web-data-viz/src/routes/empresas.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var router = express.Router();
+
+var empresaController = require("../controllers/empresaController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+router.post("/cadastrar", function (req, res) {
+    console.log('Entrei na rota')
+    empresaController.cadastrar(req, res);
+})
+
+
+module.exports = router;
