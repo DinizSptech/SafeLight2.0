78432f2b1a5fe37c8091962c815e66e8076465a5
jodroassis <joao.santos006@sptech.school>
2024-12-03 09:47:36 -0300

    Update - Dashboard Geral e Individual (Atualizar os dados)

--- Site/web-data-viz/src/models/dashboardModel.js
+++ Site/web-data-viz/src/models/dashboardModel.js
@@ -27,7 +27,7 @@ function obterDadosSensoresIndividual(idEndereco, idSensor, data) {
 	JOIN sensor as s 
 	ON d.fkSensor = s.idSensor
 	WHERE s.fkEndereco = ${idEndereco} and s.idSensor = ${idSensor} and d.momento like '%${data}%'
-    ORDER BY momento asc;
+    ORDER BY momento desc;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
@@ -39,8 +39,7 @@ function obterDadosSensores(idGerente) {
 
     var instrucaoSql = `
         SELECT 
-            d.luminosidade, 
-            d.momento, 
+            AVG(d.luminosidade) as luminosidade,  
             s.local, 
             s.andar 
         FROM 
@@ -51,8 +50,9 @@ function obterDadosSensores(idGerente) {
             endereco as e ON s.fkEndereco = e.idEndereco
         JOIN 
             gerenciadores as g ON e.idEndereco = g.fkEndereco
-        WHERE 
-            g.idGerenciadores = ${idGerente};
+		WHERE g.idGerenciadores = ${idGerente}
+		GROUP BY 
+			s.local, s.andar;
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
